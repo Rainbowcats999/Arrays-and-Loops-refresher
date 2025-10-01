41c95d5884eb0be75d4cad9cd621d43dc1b1a079
Rainbowcats999 <526erizov@frhsd.com>
2025-10-01 09:13:01 -0400

    Let's see if this works

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const foods = [
-    'sushi',
-    'pizza',
-    'burgers',
-    'ice cream',
-    'cake',
-    'noodles',
-    'chocolate',
-    'cookies',
-    'steak',
-    'mochi'];
-foods.sort();
-console.log(foods); //Output
+const foods = [ 
+            'sushi',
+            'pizza',
+            'burgers',
+            'ice cream',
+            'cake',
+            'noodles',
+            'chocolate',
+            'cookies',
+            'steak',
+            'mochi'
+        ];
+
+        foods.sort();
+
+        const listElement = document.getElementById('food-list');
+
+        // Add each food as a list item
+        for (let i = 0; i < foods.length; i++) {
+            const li = document.createElement('li');
+            li.textContent = foods[i];
+            listElement.appendChild(li);
+        }
